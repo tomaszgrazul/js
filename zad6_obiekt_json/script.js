c092097fc3eb883e13c14fc3b491460f831025db
tomaszgrazul <tgra@wp.pl>
2023-02-04 22:33:39 +0100

    zrobione

---
+++ zad6_obiekt_json/script.js
@@ -0,0 +1,37 @@
+let jsonPracownicy = {
+    "pracownicy":[
+        {"firstName": "Krystian", "lastName": "Dziopa"}, 
+        {"firstName": "Anna", "lastName": "Szapiel"},
+        {"firstName": "Piotr", "lastName": "Żmuda"}
+    ]
+}
+
+console.log(jsonPracownicy);
+
+
+// jsonPracownicy.pracownicy.forEach( function(element, index) {
+//     console.log('Index[' + index + '] imie: ' + jsonPracownicy.pracownicy[index].firstName + ', nazwisko: ' + jsonPracownicy.pracownicy[index].lastName);
+// });
+
+jsonPracownicy.pracownicy.forEach((element, index) => {
+    // console.log('Index[' + index + '] imie: ' + jsonPracownicy.pracownicy[index].firstName + ', nazwisko: ' + jsonPracownicy.pracownicy[index].lastName);
+    console.log('Index[' + index + '] ', element );
+});
+// console.log(jsonPracownicy.pracownicy[0].firstName);
+
+// let klucze = Object.keys(jsonPracownicy.pracownicy);
+// console.log(klucze);
+
+// let wartosci = Object.values(jsonPracownicy.pracownicy);
+// console.log(wartosci);
+
+// let wpisy = Object.entries(jsonPracownicy.pracownicy[0]);
+// console.log(wpisy);
+
+// for (let key in jsonPracownicy.pracownicy) {
+//     console.log(jsonPracownicy.pracownicy[key]);
+// }
+
+// for (let key1 of jsonPracownicy.pracownicy) {
+//     console.log(jsonPracownicy.pracownicy[key1]);
+// }
